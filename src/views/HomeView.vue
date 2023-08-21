c994fe8b43230c2ade53492a21c7e8c83bf125ed
songbinghui <soongbinghui@gmail.com>
2023-08-21 21:08:36 +0800

    feat: 实现echarts动画

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-chart class="chart" :option="option" :theme="theme" autoresize />
+  <v-chart class="chart" ref="pie" :theme="theme" :option="option" autoresize />
 </template>
 
 <script setup lang="ts">
@@ -8,10 +8,60 @@ import { CanvasRenderer } from 'echarts/renderers'
 import { PieChart } from 'echarts/charts'
 import { TitleComponent, TooltipComponent, LegendComponent } from 'echarts/components'
 import VChart from 'vue-echarts'
-import { ref } from 'vue'
+import { onMounted, ref, shallowRef } from 'vue'
 import { theme } from '@/composables'
+import type { ECharts } from 'echarts'
 use([CanvasRenderer, PieChart, TitleComponent, TooltipComponent, LegendComponent])
+const pie = shallowRef<ECharts>()
+let timer: number | undefined
+onMounted(() => {
+  startActions()
 
+  return () => {
+    stopActions()
+  }
+})
+
+function startActions() {
+  let dataIndex = -1
+
+  const dataLen = option.value?.series?.[0]?.data?.length || 0
+
+  if (!pie.value || dataLen === 0) {
+    return
+  }
+
+  clearInterval(timer)
+
+  timer = setInterval(() => {
+    if (!pie.value) {
+      clearInterval(timer)
+
+      return
+    }
+
+    pie.value.dispatchAction({
+      type: 'downplay',
+      seriesIndex: 0,
+      dataIndex
+    })
+    dataIndex = (dataIndex + 1) % dataLen
+    pie.value.dispatchAction({
+      type: 'highlight',
+      seriesIndex: 0,
+      dataIndex
+    })
+    pie.value.dispatchAction({
+      type: 'showTip',
+      seriesIndex: 0,
+      dataIndex
+    })
+  }, 1000)
+}
+
+function stopActions() {
+  clearInterval(timer)
+}
 const option = ref({
   title: {
     text: 'Traffic Sources',
