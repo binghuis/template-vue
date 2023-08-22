c40d0fffd0e82125deee5d65679c91768d258a20
songbinghui <soongbinghui@gmail.com>
2023-08-22 11:31:01 +0800

    feat: 图形动画修复

--- src/views/HomeView.tsx
+++ src/views/HomeView.tsx
@@ -106,7 +106,7 @@ export default defineComponent({
         <VChart
           style={{ height: '100vh' }}
           theme={theme.value}
-          ref="pie"
+          ref={pie}
           option={option}
           autoresize
         />
