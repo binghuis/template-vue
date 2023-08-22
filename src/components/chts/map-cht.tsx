c1adc94fce98ebab9558351b930b66589625a7cc
songbinghui <soongbinghui@gmail.com>
2023-08-22 19:52:41 +0800

    feat: geomap 类型问题

--- src/components/chts/map-cht.tsx
+++ src/components/chts/map-cht.tsx
@@ -5,6 +5,7 @@ import { registerMap } from 'echarts'
 import { EffectScatterChart, ScatterChart } from 'echarts/charts'
 import { GeoComponent, LegendComponent, TitleComponent, TooltipComponent } from 'echarts/components'
 import { use } from 'echarts/core'
+import type { GeoJSONSourceInput } from 'echarts/types/src/coord/geo/geoTypes'
 import { defineComponent, onBeforeUnmount, onMounted, shallowRef } from 'vue'
 import VChart from 'vue-echarts'
 import chinaMap from './map.json'
@@ -46,9 +47,8 @@ export default defineComponent({
       timer = setInterval(tick, 1000)
     }
 
-    // registerMap('china', chinaMap)
     const option = shallowRef(getData())
-    registerMap('china', chinaMap)
+    registerMap('china', chinaMap as GeoJSONSourceInput)
 
     onMounted(() => {
       http.getChinaDataVGEO().then((chinaMap) => {
