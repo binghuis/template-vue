034b201d517e090a6941eeefe568b9b21057ad19
songbinghui <soongbinghui@gmail.com>
2023-08-23 10:19:31 +0800

    feat: 地图测试

--- src/components/chts/map-cht.tsx
+++ src/components/chts/map-cht.tsx
@@ -1,14 +1,13 @@
 import { theme } from '@/composables'
 import getData from '@/data/map'
-import http from '@/service/http'
 import { registerMap, type ECharts } from 'echarts'
 import { EffectScatterChart, ScatterChart } from 'echarts/charts'
 import { GeoComponent, LegendComponent, TitleComponent, TooltipComponent } from 'echarts/components'
 import { use } from 'echarts/core'
 import type { GeoJSONSourceInput } from 'echarts/types/src/coord/geo/geoTypes'
-import { defineComponent, onBeforeUnmount, onMounted, shallowRef } from 'vue'
+import { defineComponent, shallowRef } from 'vue'
 import VChart from 'vue-echarts'
-import chinaMap from './map.json'
+import chinaMap from './china-map.noformat.json'
 export default defineComponent({
   setup() {
     use([
@@ -20,42 +19,8 @@ export default defineComponent({
       TooltipComponent
     ])
 
-    const seconds = shallowRef(0)
-    const loading = shallowRef(false)
-
-    let timer: NodeJS.Timeout
-
-    onBeforeUnmount(() => {
-      clearInterval(timer)
-    })
-
-    function tick() {
-      seconds.value--
-
-      if (seconds.value === 0) {
-        clearInterval(timer)
-        loading.value = false
-        option.value = getData()
-      }
-    }
-
-    function refresh() {
-      // simulating async data from server
-      seconds.value = 3
-      loading.value = true
-
-      timer = setInterval(tick, 1000)
-    }
-
     const option = shallowRef(getData())
     registerMap('china', chinaMap as GeoJSONSourceInput)
-
-    onMounted(() => {
-      http.getChinaDataVGEO().then((chinaMap) => {
-        // registerMap('china', chinaMap)
-        refresh()
-      })
-    })
 
     const map = shallowRef<ECharts>()
     return () => (
@@ -64,7 +29,6 @@ export default defineComponent({
         theme={theme.value}
         option={option.value}
         autoresize
-        loading={loading.value}
         style={{ height: '100vh' }}
       />
     )
